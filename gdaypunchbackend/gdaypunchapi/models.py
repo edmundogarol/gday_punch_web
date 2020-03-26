ce3877d5295b86aa147177ddc161072b537bf37f
edmundogarol <edmundo.a.garol@outlook.com>
2020-03-26 17:40:46 +1100

    Added roles field in user model to fascilitate different user types in the future - including editors, artists and admin

--- gdaypunchbackend/gdaypunchapi/models.py
+++ gdaypunchbackend/gdaypunchapi/models.py
@@ -1,7 +1,27 @@
 from django.db import models
-# from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser
 
-# class User(AbstractUser):
-#     bio = models.TextField(max_length=500, blank=True)
-#     location = models.CharField(max_length=30, blank=True)
-#     birth_date = models.DateField(null=True, blank=True)
+class Role(models.Model):
+  '''
+  The Role entries are managed by the system,
+  automatically created via a Django data migration.
+  '''
+  ARTIST = 1
+  EDITOR = 2
+  ADMIN = 3
+  ROLE_CHOICES = (
+      (ARTIST, 'artist'),
+      (EDITOR, 'editor'),
+      (ADMIN, 'admin'),
+  )
+
+  id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
+
+  def __str__(self):
+      return self.get_id_display()
+
+class User(AbstractUser):
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    roles = models.ManyToManyField(Role)
