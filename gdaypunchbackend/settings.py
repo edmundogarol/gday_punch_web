9dbcf55631ad1d9698f5ca7a5a8f658f56a84865
edmundogarol <edmundo.a.garol@outlook.com>
2020-05-01 23:53:08 +1000

    Add ELB host checker script to allowe health checker ip

--- gdaypunchbackend/settings.py
+++ gdaypunchbackend/settings.py
@@ -31,6 +31,39 @@ ALLOWED_HOSTS = [
     "0.0.0.0",
 ]
 
+def is_ec2_linux():
+    """Detect if we are running on an EC2 Linux Instance
+       See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+    """
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
+def get_linux_ec2_private_ip():
+    """Get the private IP Address of the machine if running on an EC2 linux server"""
+    from urllib.request import urlopen
+    if not is_ec2_linux():
+        return None
+    try:
+        response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
+        return response.read()
+    except:
+        return None
+    finally:
+        if response:
+            response.close()
+
+# ElasticBeanstalk healthcheck sends requests with host header = internal ip
+# So we detect if we are in elastic beanstalk,
+# and add the instances private ip address
+private_ip = get_linux_ec2_private_ip()
+print(private_ip)
+if private_ip:
+    ALLOWED_HOSTS.append(private_ip)
+    
+print(ALLOWED_HOSTS)
+
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
