2d9c37bf6a3ea684f6fe41416b24b949f68efc1b
edmundogarol <edmundo.a.garol@outlook.com>
2020-05-09 20:14:02 +1000

    Removed patcher code

--- gdaypunchbackend/settings.py
+++ gdaypunchbackend/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .utils.get_eb_env import patch_environment
 import json
-
-patch_environment()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
