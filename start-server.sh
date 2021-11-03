781d5d3776b43db2d713ef0d100766ae5a1befa9
edmundogarol <edmundo.a.garol@outlook.com>
2021-11-03 21:23:16 +1100

    Add log debug in gunicorn command

--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # start-server.sh
-(cd gdaypunchbackend; PYTHONPATH=`pwd`/.. gunicorn gdaypunchbackend.wsgi:application www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd gdaypunchbackend; PYTHONPATH=`pwd`/.. gunicorn gdaypunchbackend.wsgi:application www-data --bind 0.0.0.0:8010 --workers 3 --threads 1 --log-level debug) &
 nginx -g "daemon off;"
