a578ee377943ebfc25632061a9e026b9e33b53d8
edmundogarol <edmundo.a.garol@outlook.com>
2022-02-14 10:45:11 +1000

    Add order number and order name searcher in account seller view

--- gdaypunchwebapp/gdaypunchreact/src/pages/Account/seller/index.js
+++ gdaypunchwebapp/gdaypunchreact/src/pages/Account/seller/index.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { Card, Table, Result, Button } from "antd";
+import { Table, Input, Button, Space, Result, Card } from "antd";
+import Highlighter from "react-highlight-words";
+
 import { isEmpty } from "lodash";
 import {
   ClockCircleOutlined as PendingIcon,
@@ -9,6 +11,7 @@ import {
   PlayCircleOutlined as ShippedIcon,
   InfoCircleOutlined as RefundedIcon,
   CloseCircleOutlined as DeclinedIcon,
+  SearchOutlined,
 } from "@ant-design/icons";
 
 import LoadingSpinner from "components/loadingSpinner";
@@ -29,11 +32,11 @@ function Seller(props) {
     sellerDetails,
     fetchingSellerDetails,
     finishedFetchingSellerDetails,
-    sellerDetailsError,
+    // sellerDetailsError,
     sellerSales,
     fetchingSellerSales,
     finishedFetchingSellerSales,
-    sellerSalesError,
+    // sellerSalesError,
     finishedUpdatingSellerDetails,
     sellerDetailsUpdateError,
   } = useSelector(selectSellerState);
@@ -41,6 +44,10 @@ function Seller(props) {
   const dispatch = useDispatch();
 
   const [sellerDetailsOpen, toggleSellerDetailsOpen] = useState(false);
+
+  const [searchInput, updateSearchInput] = useState(undefined);
+  const [searchText, updateSearchText] = useState("");
+  const [searchedColumn, updateSearchedColumn] = useState("");
 
   useScrollTop();
 
@@ -78,6 +85,149 @@ function Seller(props) {
     }
   }, [finishedUpdatingSellerDetails]);
 
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    updateSearchText(selectedKeys[0]);
+    updateSearchedColumn(dataIndex);
+  };
+
+  const handleReset = (clearFilters) => {
+    clearFilters();
+    updateSearchText("");
+  };
+
+  const getColumnSearchProps = (dataIndex, mobile) => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters,
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={(node) => {
+            updateSearchInput(node);
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+          style={{ marginBottom: 8, display: "block" }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => {
+              handleSearch(selectedKeys, confirm, dataIndex);
+            }}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              updateSearchText(selectedKeys[0]);
+              updateSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered) => (
+      <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+    ),
+    onFilter: (value, instance) => {
+      const searchingNames = dataIndex === "name";
+      let valueToSearch = instance[dataIndex];
+
+      if (searchingNames) {
+        valueToSearch = instance.first_name
+          ? `${instance.first_name} ${instance.last_name}`
+          : instance.email;
+      }
+
+      return (searchingNames ? searchingNames : instance[dataIndex])
+        ? valueToSearch.toLowerCase().includes(value.toLowerCase())
+        : "";
+    },
+    onFilterDropdownVisibleChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.select(), 100);
+      }
+    },
+    render: (text, instance) => {
+      const renderHighliter = (highlighted) => {
+        let textToUse = text;
+
+        if (dataIndex === "name") {
+          textToUse = instance.first_name
+            ? `${instance.first_name} ${instance.last_name}`
+            : instance.email;
+        }
+        if (dataIndex === "number") {
+          textToUse = `#${text}`;
+        }
+
+        if (highlighted) {
+          return (
+            <Highlighter
+              highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+              searchWords={[searchText]}
+              autoEscape
+              textToHighlight={textToUse ? textToUse.toString() : ""}
+            />
+          );
+        } else {
+          return <span>{textToUse}</span>;
+        }
+      };
+
+      const renderColumn = (isMobile) => {
+        if (dataIndex === "number" && isMobile) {
+          return (
+            <div className="detail-3-column-compressed">
+              <p>{renderHighliter(searchedColumn === dataIndex)}</p>
+              <p>{instance.readable_date.date}</p>
+              <p>{instance.fulfillment_type}</p>
+            </div>
+          );
+        } else if (dataIndex === "name" && isMobile) {
+          const StatusIcon = renderStatusIcons[instance.status];
+
+          return (
+            <div className="detail-3-column-compressed">
+              <p>{renderHighliter(searchedColumn === dataIndex)}</p>
+              <p>{`A$${instance.amount.toFixed(2)}`}</p>
+              <span className={instance.status}>
+                <StatusIcon /> {instance.status}
+              </span>
+            </div>
+          );
+        } else {
+          return renderHighliter(searchedColumn === dataIndex);
+        }
+      };
+
+      return renderColumn(mobile);
+    },
+  });
+
   const renderStatusIcons = {
     pending: PendingIcon,
     purchased: PurchasedIcon,
@@ -99,16 +249,14 @@ function Seller(props) {
       title: "Order No.",
       dataIndex: "number",
       key: "number",
-      render: (number, order) => {
-        const StatusIcon = renderStatusIcons[order.status];
-
-        return (
-          <span className={`status ${order.status}`}>
-            {`#${number}`}
-            <StatusIcon />
-          </span>
-        );
-      },
+      ...getColumnSearchProps("number"),
+    },
+    {
+      title: "Customer",
+      dataIndex: "name",
+      key: "name",
+      className: "email-or-name",
+      ...getColumnSearchProps("name"),
     },
     {
       title: "Items",
